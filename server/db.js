77c6361232f1f2fe4ed9625604271901249ec61f
patriciosebastian <psebastiansalazar@gmail.com>
2023-09-21 11:59:31 -0700

    update db connection strings for railway

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
-// not a typo
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
 });
 
 module.exports = pool;
