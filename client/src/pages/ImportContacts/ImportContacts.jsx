f39674024e84fac73807d52d78b2b5e4219b2cdf
patriciosebastian <psebastiansalazar@gmail.com>
2024-09-19 23:42:29 -0500

    corrected where I set loading state in import contacts and implemented toast alerts

--- client/src/pages/ImportContacts/ImportContacts.jsx
+++ client/src/pages/ImportContacts/ImportContacts.jsx
@@ -4,18 +4,23 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { useAuth } from "../../context/AuthContext";
 import { useState } from "react";
 import "./ImportContacts.css";
+import AlertToast from "../../components/AlertToast/AlertToast";
 
 export default function ImportContacts() {
   const isDesktop = useMedia("(min-width: 1200px)");
   const { idToken } = useAuth();
   const [loading, setLoading] = useState(false);
+  const [toastAlert, setToastAlert] = useState({
+    visible: false,
+    message: "",
+    type: "",
+  });
 
   const handleFileUpload = (e) => {
     setLoading(true);
 
     const file = e.target.files[0];
 
-    // remember to use loading state
     if (file) {
       const formData = new FormData();
       formData.append("file", file);
@@ -32,15 +37,31 @@ export default function ImportContacts() {
       .then(response => response.json())
       .then(data => {
         console.log('Success:', data);
+        setLoading(false);
+        setToastAlert({
+          visible: true,
+          message: 'Contacts imported successfully!',
+          type: 'success'
+        });
       })
       .catch((error) => {
         console.error('Error:', error);
+        setLoading(false);
+        setToastAlert({
+          visible: true,
+          message: 'Error importing contacts. Please refresh the page and try again.',
+          type: 'error'
+        });
       });
     } else {
       console.log('No file selected');
       setLoading(false);
+      setToastAlert({
+        visible: true,
+        message: 'No file selected. Please select a file and try again.',
+        type: 'error'
+      });
     }
-    setLoading(false);
   };
 
   return (
@@ -48,10 +69,12 @@ export default function ImportContacts() {
       <h1 className='import-contacts-main-heading'>Import Contacts</h1>
       {isDesktop && <SideNav className='import-contacts-side-nav' />}
 
-      {loading ? <LoadingSpinner />
+      {loading ?
+        <LoadingSpinner />
         :
         <input className="import-contacts-input" type="file" accept=".csv" onChange={handleFileUpload} />
       }
+      {toastAlert.visible && <AlertToast type={toastAlert.type} message={toastAlert.message} onDismiss={() => setToastAlert(prev => ({ ...prev, visible: false }))} />}
     </div>
   );
 }
