30047f963b8a7d6486f4c016c456c7c72fb5d9dd
Patricio Salazar <psebastiansalazar@gmail.com>
2025-11-19 15:20:17 -0800

    clean up notes

--- server/index.js
+++ server/index.js
@@ -20,11 +20,6 @@ app.use('/groups', groupRoutes);
 app.use('/app', emailRoutes);
 app.use('/demo', demoRoutes);
 
-// TODO: Rate Limiting and Size Checks: Implement rate limiting and file size checks to prevent abuse and ensure that my service can handle the load.
-// TODO: Security: Ensure that the file being processed is indeed a CSV file and does not contain malicious code. This can be part of my validation step.
-// TODO: Validate CSV Data: Before inserting the data into your database, validate the CSV data to ensure it meets my application's and database's constraints, such as required fields, data types, and value ranges.
-// TODO: File Cleanup: If you're creating temporary files or need to clean up resources after processing (for files that are downloaded rather than streamed), ensure I have a mechanism in place to do so.
-
 const PORT = process.env.PORT || 5300;
 
 app.listen(PORT, () => {
